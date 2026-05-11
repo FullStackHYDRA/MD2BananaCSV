10f2d9c32b234b132fa87bb4747dc9ca4d4a6728
FullStackHYDRA <andrew.egli@hotmail.com>
2026-05-11 10:41:18 +0200

    Feat: um Markdown files zu CSV files zu convertieren.

---
+++ md2csv.sh
@@ -0,0 +1,16 @@
+
+if [ -z "$1"]; then
+    echo "Nutzt: $0 dateiname.md"
+    exit
+fi
+
+INPUT_FILE="$1"
+
+OUTPUT_FILE="${INPUT_FILE%.md}.csv"
+
+
+grep "^| " "$INPUT_FILE" | \
+grep -v "\-|" | \
+sed 's/^|//; s/|$//; s/|/,/g; s/s *, */,/g; s/^ *//; s/ *$//' > "$OUTPUT_FILE"
+
+echo "Datei erfolgreich erstellt: $OUTPUT_FILE"
