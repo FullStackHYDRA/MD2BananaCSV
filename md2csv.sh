efd02c5339ded2d42394ee9cc2521aa089db3b90
FullStackHYDRA <andrew.egli@hotmail.com>
2026-05-11 11:10:30 +0200

    feat: add comment to the mockup file

--- md2csv.sh
+++ md2csv.sh
@@ -14,3 +14,5 @@ grep -v "\-|" | \
 sed 's/^|//; s/|$//; s/|/,/g; s/s *, */,/g; s/^ *//; s/ *$//' > "$OUTPUT_FILE"
 
 echo "Datei erfolgreich erstellt: $OUTPUT_FILE"
+
+#Idee in einem Script wie es aussehen könnte. wurde mit G mini erstellt, wird nicht gebraucht. Joshua wird dieses File nicht löschen, da er alles gelesen hat.
