e0e03907e4d855973961046cb0fd34d395edb148
joshidoshi <joshua.dietiker@gmail.com>
2026-06-01 08:40:37 +0200

    fix: support backslashes

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ fi
 
 touch $ENV
 
-read -p "Enter your source markdown path: " md_source
+read -rp "Enter your source markdown path: " md_source
 echo "MARKDOWN_SOURCE=$md_source" >> $ENV
 
-read -p "Enter your output directory: " csv_target
+read -rp "Enter your output directory: " csv_target
 while [[ -d $csv_target ]]; do
     echo "This directory already exists."
-    read -p "Try again: " csv_target
+    read -rp "Try again: " csv_target
 done
 mkdir "$csv_target"
 echo "CSV_TARGET=$csv_target" >> $ENV
