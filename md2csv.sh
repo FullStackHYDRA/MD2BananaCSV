10d65a432513f85bee9e227542c3f51bab6fc1f3
joshidoshi <joshua.dietiker@gmail.com>
2026-06-01 08:52:23 +0200

    feat: create multiple folders if there are multiple following tags

--- md2csv.sh
+++ md2csv.sh
@@ -1,7 +1,8 @@
 source .env.banana
 
 CSV_CONTENT=$(awk -F':: ' '/^- / {gsub(/[[:space:]]*#card.*/, ""); gsub(/^- /, ""); q=$0; next} /card-/ {next} /^[[:space:]]+-/ {gsub(/^[[:space:]]+-\s*/, ""); print "\"" q "\",\"" $0 "\""}' "$MARKDOWN_SOURCE")
-TAG=$(awk -F' ' '/^- .*#card/ {print $NF; exit}' "$MARKDOWN_SOURCE" | sed 's/#//')
+TAG_PATH=$(awk '/^- .*#card/ {sub(/.*#card /, ""); print; exit}' "$MARKDOWN_SOURCE" | sed 's/#//g; s/[[:space:]][[:space:]]*/\//g')
 
-mkdir -p "$CSV_TARGET/$TAG"
-echo "$CSV_CONTENT" > "$CSV_TARGET/$TAG/output.csv"
+TARGET_PATH="$CSV_TARGET/$TAG_PATH"
+mkdir -p "$TARGET_PATH"
+echo "$CSV_CONTENT" > "$TARGET_PATH/output.csv"
