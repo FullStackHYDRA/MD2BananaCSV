c69f502bd7f1714b1851fcc11b587cfd165d96ea
joshidoshi <joshua.dietiker@gmail.com>
2026-06-01 08:46:23 +0200

    refactor: simplify file path

--- md2csv.sh
+++ md2csv.sh
@@ -3,6 +3,5 @@ source .env.banana
 CSV_CONTENT=$(awk -F':: ' '/^- / {gsub(/[[:space:]]*#card.*/, ""); gsub(/^- /, ""); q=$0; next} /card-/ {next} /^[[:space:]]+-/ {gsub(/^[[:space:]]+-\s*/, ""); print "\"" q "\",\"" $0 "\""}' "$MARKDOWN_SOURCE")
 TAG=$(awk -F' ' '/^- .*#card/ {print $NF; exit}' "$MARKDOWN_SOURCE" | sed 's/#//')
 
-mkdir -p "$CSV_TARGET" && cd $CSV_TARGET
-mkdir -p "$TAG" && cd $TAG
-echo "$CSV_CONTENT" > "output.csv"
+mkdir -p "$CSV_TARGET/$TAG"
+echo "$CSV_CONTENT" > "$CSV_TARGET/$TAG/output.csv"
