9ebce603b761ac7190b12faa68d607eaa00510fe
joshidoshi <joshua.dietiker@gmail.com>
2026-06-01 10:41:56 +0200

    feat: use directory instead of single file for input

--- md2csv.sh
+++ md2csv.sh
@@ -1,8 +1,12 @@
 source .env.banana
+shopt -s globstar
 
-CSV_CONTENT=$(awk -v sep="$CSV_SEPARATOR" -F':: ' '/^- / {gsub(/[[:space:]]*#card.*/, ""); gsub(/^- /, ""); q=$0; next} /card-/ {next} /^[[:space:]]+-/ {gsub(/^[[:space:]]+-\s*/, ""); print "\"" q "\"" sep "\"" $0 "\""}' "$MARKDOWN_SOURCE")
-TAG_PATH=$(awk '/^- .*#card/ {sub(/.*#card /, ""); print; exit}' "$MARKDOWN_SOURCE" | sed 's/#//g; s/[[:space:]][[:space:]]*/\//g')
+for file in "$MARKDOWN_SOURCE"/**/*.md; do
+    CSV_CONTENT=$(awk -v sep="$CSV_SEPARATOR" -F':: ' '/^- / {gsub(/[[:space:]]*#card.*/, ""); gsub(/^- /, ""); q=$0; next} /card-/ {next} /^[[:space:]]+-/ {gsub(/^[[:space:]]+-\s*/, ""); print "\"" q "\"" sep "\"" $0 "\""}' "$file")
 
-TARGET_PATH="$CSV_TARGET/$TAG_PATH"
-mkdir -p "$TARGET_PATH"
-echo "$CSV_CONTENT" > "$TARGET_PATH/output.csv"
+    TAG_PATH=$(awk '/^- .*#card/ {sub(/.*#card /, ""); print; exit}' "$file" | sed 's/#//g; s/[[:space:]][[:space:]]*/\//g')
+    TARGET_PATH="$CSV_TARGET/$TAG_PATH"
+    mkdir -p "$TARGET_PATH"
+
+    echo "$CSV_CONTENT" > "$TARGET_PATH/output.csv"
+done
