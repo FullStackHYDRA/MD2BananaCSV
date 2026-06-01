71886022fb2daff13fdb7274718c384cf6a867ac
joshidoshi <joshua.dietiker@gmail.com>
2026-06-01 10:46:19 +0200

    feat: name files individually, only take such with a card tag

--- md2csv.sh
+++ md2csv.sh
@@ -2,11 +2,12 @@ source .env.banana
 shopt -s globstar
 
 for file in "$MARKDOWN_SOURCE"/**/*.md; do
+    grep -q '#card' "$file" || continue
     CSV_CONTENT=$(awk -v sep="$CSV_SEPARATOR" -F':: ' '/^- / {gsub(/[[:space:]]*#card.*/, ""); gsub(/^- /, ""); q=$0; next} /card-/ {next} /^[[:space:]]+-/ {gsub(/^[[:space:]]+-\s*/, ""); print "\"" q "\"" sep "\"" $0 "\""}' "$file")
 
     TAG_PATH=$(awk '/^- .*#card/ {sub(/.*#card /, ""); print; exit}' "$file" | sed 's/#//g; s/[[:space:]][[:space:]]*/\//g')
     TARGET_PATH="$CSV_TARGET/$TAG_PATH"
     mkdir -p "$TARGET_PATH"
 
-    echo "$CSV_CONTENT" > "$TARGET_PATH/output.csv"
+    echo "$CSV_CONTENT" > "$TARGET_PATH/$(basename "${file%.*}").csv"
 done
