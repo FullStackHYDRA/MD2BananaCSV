21541fb3b7a286d9126b05ef4a53b2015789d867
Joshua D <joshua.dietiker@gmail.com>
2026-06-04 13:20:00 +0200

    feat: install modules required for autosync
    user must have sudo rights

--- setup.sh
+++ setup.sh
@@ -7,21 +7,27 @@ fi
 
 touch $ENV
 
-read -rp "Enter your source markdown path: " md_source
-echo "MARKDOWN_SOURCE=$md_source" >> $ENV
+read -e -rp "Enter your source markdown path: " md_source
+echo "MARKDOWN_SOURCE=\"$md_source\"" >> $ENV
 
-read -rp "Enter your output directory: " csv_target
+read -e -rp "Enter your output directory: " csv_target
 mkdir -p "$csv_target"
-echo "CSV_TARGET=$csv_target" >> $ENV
+echo "CSV_TARGET=\"$csv_target\"" >> $ENV
 
-read -p "Enter your CSV separator (| , ;): " csv_separator
+read -e -p "Enter your CSV separator (| , ;): " csv_separator
 echo "CSV_SEPARATOR=\"$csv_separator\"" >> $ENV
 
-read -p "Enable autosync (y/n): " askyesno_autosync
-if [ "$askyesno_autosync" -eq "y" ]; then
-    npm install -g chokidar-cli
-    bash autosync.sh > autosync.log 2>&1 &
+read -e -p "Enable autosync (y/n): " askyesno_autosync
+if [ "$askyesno_autosync" == "y" ]; then
+    echo && echo "Installing npm..."
+    sudo apt-get install -y npm > /dev/null
+
+    echo "Installing chokidar..."
+    sudo npm install -g chokidar-cli > /dev/null
+
+    nohup bash autosync.sh >/dev/null 2>&1 &
+
+    echo "Enabled autosync." && echo
 fi
-echo "Enabled autosync."
 
 echo "Successfully set up MD2BananaCSV!"
